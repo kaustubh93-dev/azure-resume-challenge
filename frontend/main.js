35d6d4962d5eb920244ad0e064c1942a8be302ed
Kaustubh Sharma <55542518+kaustubh93-dev@users.noreply.github.com>
2024-03-29 15:44:42 +0530

    init

--- frontend/main.js
+++ frontend/main.js
@@ -1,21 +1,38 @@
-window.addEventListener('DOMContentLoaded' , (Event) =>{
-    getvisitcount();
-})
+const productionApiUrl = 'http://localhost:7071/api/ResumeCounter';
 
-const functionapi ='';
+function getOrdinalSuffix(number) {
+  const lastDigit = number % 10;
+  const lastTwoDigits = number % 100;
 
-const getvisitcount = () =>{
-     let count =30;
-     fetch (functionapi).then(Response => {
-          return Response.json() 
-     }).then (Response => {
-          console.log ("Website called function api.");
-          count = Response.count;
-          document.getElementById("counter").innerText =count;
-     }).catch(function(error){
-    console.log(error);
-     });
-    return count;
+  if (lastTwoDigits >= 11 && lastTwoDigits <= 13) {
+    return 'th';
+  }
+
+  switch (lastDigit) {
+    case 1:
+      return 'st';
+    case 2:
+      return 'nd';
+    case 3:
+      return 'rd';
+    default:
+      return 'th';
+  }
 }
 
-    
+async function getVisitCountAndUpdate() {
+  try {
+    const response = await fetch(productionApiUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const responseData = await response.json(); // Parse response as JSON
+    const visitCount = responseData.count;
+    const suffix = getOrdinalSuffix(visitCount);
+    document.getElementById('counter').textContent = `${visitCount}${suffix}`;
+  } catch (error) {
+    console.error('Error fetching and updating visit count ', error);
+  }
+}
+
+getVisitCountAndUpdate();
